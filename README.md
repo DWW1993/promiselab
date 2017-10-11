74fb4e8524f0823ddf7dddaaabe45996de4a952f
DWW1993 <daniel.w.walker1993@gmail.com>
2017-10-10 19:47:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Warm up
+
+Fulfill a promise
+
+Reject a promise
+
+To reject or not to reject
+
+Always asynchronous
+
+Shortcuts
+
+Promise after promise
+
+Values and promises
---
+++ warmup.js
@@ -0,0 +1,9 @@
+'use strict';
+ var promise = new Promise(function (fulfill, reject) {
+    // Your solution here
+        setTimeout(function () { 
+            
+        }, 300)
+});
+
+// Your solution here
